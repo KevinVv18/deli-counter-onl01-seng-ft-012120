5c902f3527077b77032594a09fcace54677edd50
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-25 02:52:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,6 @@ def line(katz_deli)
   katz_deli= []
   counter= 0
   puts  "The line is currently empty."
-while katz_deli.size > counter
-  counter =+ 1
-  puts "Te line is currently: #{counter}. #{katz_deli}."
-end
+def line(other_dali)
 
-end
+other_dali.each |name| di
