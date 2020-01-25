7a1fc814fb0c8fa3231f939e9639e83a966d2b11
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-25 02:25:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,10 @@
 def line(name)
   counter ==0 
 while counter =< name.lenght
-puts ["The line is currently "]
+puts ["The line is currently : #[counter]. #[name] "]
+
+else counter= 0 
+  
+  puts "The line is currently empty"
+end   
+end
