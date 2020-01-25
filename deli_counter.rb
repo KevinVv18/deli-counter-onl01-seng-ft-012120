4446c6653055e5b017cc3e470ca81b74b51af4f6
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-25 02:58:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,3 +3,13 @@ def line(katz_deli)
   puts  "The line is currently empty."
 end
 
+
+def line(other_dali)
+other_dali= i
+counter= 1
+other_dali.each do |other_dali|
+  puts "The line is currently: #{counter}. #{other_dali}."
+  counter+= 1 
+end 
+
+end
