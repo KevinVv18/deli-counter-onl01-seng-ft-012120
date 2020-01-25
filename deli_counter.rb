1b57b2cf5ea061b76d95a22a261a0240184be0aa
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-25 03:03:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,8 @@ counter= 1
 other_dali.each do |other_dali|
    counter+= 1 
   puts "The line is currently: #{counter}. #{other_dali}."
+  puts "The line is currently: #{counter}. #{other_dali}."
+  puts "The line is currently: #{counter}. #{other_dali}."
  
 end 
 
