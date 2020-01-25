71f5f9fe668b74fd220056ac934b4fe65fc55364
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-25 02:46:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
   katz_deli= []
   counter= 0
   puts  "The line is currently empty."
-if katz_deli.size > counter
+when katz_deli.size > counter
   counter =+ 1
   puts "Te line is currently: #{counter}. #{katz_deli}."
 end
