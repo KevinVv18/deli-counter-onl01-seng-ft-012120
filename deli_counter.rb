a792d396f257eddaac88bc3986e0e907cca178bd
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-25 02:32:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,5 @@
 def line(array, katz_deli)
   katz_deli= []
-
-while katz_deli <= array.size
-puts  "The line is currently: #[counter]. #[name]"
-end
-"The line is currently"
+  
+  puts  "The line is currently empty."
 end
