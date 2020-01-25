8af4066756737db72db82420dff92cbdb6fdb25d
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-25 02:29:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,8 @@
 def line(name)
-  counter =0 
+  counter = 0 
   size= name.size
 while counter <= size
-puts ["The line is currently : #[counter]. #[name] "]
-
+puts  "The line is currently: #[counter]. #[name]"
 end
 "The line is currently"
 end
