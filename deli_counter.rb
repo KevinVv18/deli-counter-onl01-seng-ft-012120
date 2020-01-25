e549b3afac7e7a1119ec0babb1cc28689cc0cf2d
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-25 02:42:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   counter= 1 
   puts  "The line is currently empty."
 if katz_deli.size > 0 
-  counter += 1 
+  counter =+ 1
   puts "Te line is currently: #{counter}. #{name}." 
 end
 
